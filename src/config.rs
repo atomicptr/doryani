744bf1e8974dbcf00301e3a73a2cb77b2d240fd4
Christopher Kaster <me@atomicptr.de>
2025-01-13 05:22:23 +0900

    add new NeverSink filters

--- src/config.rs
+++ src/config.rs
@@ -20,10 +20,65 @@ pub struct Filter {
 
 fn defaults() -> Vec<Filter> {
     vec![
+        // NeverSink
         Filter {
-            name: Some("NeverSinks Litefilter".to_string()),
-            url: "https://raw.githubusercontent.com/NeverSinkDev/NeverSink-PoE2litefilter/refs/heads/main/NeverSinks%20Litefilter.filter".to_string(),
+            name: Some("NeverSinks Filter - 0 Soft".to_string()),
+            url: "https://raw.githubusercontent.com/NeverSinkDev/NeverSink-Filter-for-PoE2/refs/heads/main/NeverSink's%20filter%202%20-%200-SOFT.filter".to_string(),
         },
+        Filter {
+            name: Some("NeverSinks Filter - 1 Regular".to_string()),
+            url: "https://raw.githubusercontent.com/NeverSinkDev/NeverSink-Filter-for-PoE2/refs/heads/main/NeverSink's%20filter%202%20-%201-REGULAR.filter".to_string(),
+        },
+        Filter {
+            name: Some("NeverSinks Filter - 2 Semi Strict".to_string()),
+            url: "https://raw.githubusercontent.com/NeverSinkDev/NeverSink-Filter-for-PoE2/refs/heads/main/NeverSink's%20filter%202%20-%202-SEMI-STRICT.filter".to_string(),
+        },
+        Filter {
+            name: Some("NeverSinks Filter - 3 Strict".to_string()),
+            url: "https://raw.githubusercontent.com/NeverSinkDev/NeverSink-Filter-for-PoE2/refs/heads/main/NeverSink's%20filter%202%20-%203-STRICT.filter".to_string(),
+        },
+        Filter {
+            name: Some("NeverSinks Filter - 4 Very Strict".to_string()),
+            url: "https://raw.githubusercontent.com/NeverSinkDev/NeverSink-Filter-for-PoE2/refs/heads/main/NeverSink's%20filter%202%20-%204-VERY-STRICT.filter".to_string(),
+        },
+        Filter {
+            name: Some("NeverSinks Filter - 5 Uber Strict".to_string()),
+            url: "https://raw.githubusercontent.com/NeverSinkDev/NeverSink-Filter-for-PoE2/refs/heads/main/NeverSink's%20filter%202%20-%205-UBER-STRICT.filter".to_string(),
+        },
+        Filter {
+            name: Some("NeverSinks Filter - 6 Uber Plus Strict".to_string()),
+            url: "https://github.com/NeverSinkDev/NeverSink-Filter-for-PoE2/blob/main/NeverSink's%20filter%202%20-%206-UBER-PLUS-STRICT.filter".to_string(),
+        },
+        // NeverSink Dark Mode
+        Filter {
+            name: Some("NeverSinks Filter Dark - 0 Soft".to_string()),
+            url: "https://raw.githubusercontent.com/NeverSinkDev/NeverSink-Filter-for-PoE2/refs/heads/main/(STYLE)%20DARKMODE/NeverSink's%20filter%202%20-%200-SOFT%20(darkmode)%20.filter".to_string(),
+        },
+        Filter {
+            name: Some("NeverSinks Filter Dark - 1 Regular".to_string()),
+            url: "https://raw.githubusercontent.com/NeverSinkDev/NeverSink-Filter-for-PoE2/refs/heads/main/(STYLE)%20DARKMODE/NeverSink's%20filter%202%20-%201-REGULAR%20(darkmode)%20.filter".to_string(),
+        },
+        Filter {
+            name: Some("NeverSinks Filter Dark - 2 Semi Strict".to_string()),
+            url: "https://raw.githubusercontent.com/NeverSinkDev/NeverSink-Filter-for-PoE2/refs/heads/main/(STYLE)%20DARKMODE/NeverSink's%20filter%202%20-%202-SEMI-STRICT%20(darkmode)%20.filter".to_string(),
+        },
+        Filter {
+            name: Some("NeverSinks Filter Dark - 3 Strict".to_string()),
+            url: "https://raw.githubusercontent.com/NeverSinkDev/NeverSink-Filter-for-PoE2/refs/heads/main/(STYLE)%20DARKMODE/NeverSink's%20filter%202%20-%203-STRICT%20(darkmode)%20.filter".to_string(),
+        },
+        Filter {
+            name: Some("NeverSinks Filter Dark - 4 Very Strict".to_string()),
+            url: "https://raw.githubusercontent.com/NeverSinkDev/NeverSink-Filter-for-PoE2/refs/heads/main/(STYLE)%20DARKMODE/NeverSink's%20filter%202%20-%204-VERY-STRICT%20(darkmode)%20.filter".to_string(),
+        },
+        Filter {
+            name: Some("NeverSinks Filter Dark - 5 Uber Strict".to_string()),
+            url: "https://raw.githubusercontent.com/NeverSinkDev/NeverSink-Filter-for-PoE2/refs/heads/main/(STYLE)%20DARKMODE/NeverSink's%20filter%202%20-%205-UBER-STRICT%20(darkmode)%20.filter".to_string(),
+        },
+        Filter {
+            name: Some("NeverSinks Filter Dark - 6 Uber Plus Strict".to_string()),
+            url: "https://raw.githubusercontent.com/NeverSinkDev/NeverSink-Filter-for-PoE2/refs/heads/main/(STYLE)%20DARKMODE/NeverSink's%20filter%202%20-%206-UBER-PLUS-STRICT%20(darkmode)%20.filter".to_string(),
+        },
+        // Pecham
         Filter {
             name: Some("Pechams Loot Filter - Early Game".to_string()),
             url: "https://pastebin.com/raw/8RCBmSYK".to_string(),
